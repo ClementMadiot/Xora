542696a660a15e71e679d917a486adb1ec3e0ade
ClementMadiot <clementmadiot09@gmail.com>
2024-12-20 10:37:40 -0500

    update index html & add image

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1 className='text-3xl font-bold'>Test</h1>
+      <image src="../public/images/bg-outlines-fill.png" />
+    </div>
   )
 }
 
