dfd75b21f39501b8eedf2af8587e851a81725326
ClementMadiot <clementmadiot09@gmail.com>
2024-12-21 12:16:56 -0500

    pricing card , logo & title

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { useState } from "react";
 import { Element, Link as LinkScroll } from "react-scroll";
+import { plans } from "../constants";
 
 const Pricing = () => {
   const [monthly, setMonthly] = useState(false);
@@ -14,11 +15,89 @@ const Pricing = () => {
             </h3>
             {/* buttons pricing */}
             <div className="relative z-4 mx-auto flex w-[375px] rounded-3xl border-[3px] border-s4/25 bg-s1/50 p-2 backdrop-blur-[6px] max-md:w-[310px]">
-              <button className={clsx("pricing-head_btn", monthly && "text-p4")} onClick={() => setMonthly(true)}>Monthly</button>
-              <button className={clsx("pricing-head_btn", !monthly && "text-p4")} onClick={() => setMonthly(false)}>Annual</button>
-            {/* buttons switcher  */}
-              <div className={clsx("g4 rounded-14 before:h-100 pricing-head_btn_before absolute left-2 top-2 h-[calc(100%-16px)] w-[calc(50%-8px)] overflow-hidden shadow-400 transition-transform duration-500", !monthly && "translate-x-full")} />
+              <button
+                className={clsx("pricing-head_btn", monthly && "text-p4")}
+                onClick={() => setMonthly(true)}
+              >
+                Monthly
+              </button>
+              <button
+                className={clsx("pricing-head_btn", !monthly && "text-p4")}
+                onClick={() => setMonthly(false)}
+              >
+                Annual
+              </button>
+              {/* buttons switcher  */}
+              <div
+                className={clsx(
+                  "g4 rounded-14 before:h-100 pricing-head_btn_before absolute left-2 top-2 h-[calc(100%-16px)] w-[calc(50%-8px)] overflow-hidden shadow-400 transition-transform duration-500",
+                  !monthly && "translate-x-full"
+                )}
+              />
             </div>
+            {/* pricing background   */}
+            <div className="pricing-bg">
+              <img
+                src="/public/images/bg-outlines.svg"
+                alt="outline-background"
+                width={960}
+                height={380}
+                className="relative z-2"
+              />
+              <img
+                src="/public/images/bg-outlines-fill.png"
+                alt="outline-background"
+                width={960}
+                height={380}
+                className="absolute inset-0 opacity-5 mix-blend-soft-light"
+              />
+            </div>
+          </div>
+          {/* pricing cards */}
+          <div className="scroll-hide relative z-2 -mt-12 flex items-center max-lg:gap-5 max-xl:overflow-auto max-xl:pt-6">
+            {plans.map((plan, index) => (
+              <div
+                key={plan.id}
+                className="pricing-plan_first pricing-plan_last pricing-plan_odd pricing-plan_even relative border-2 p-7 max-xl:min-w-80 max-lg:rounded-3xl xl:w-[calc(33.33%+2px)]"
+              >
+                {index === 1 && (
+                  <div className="g4 absolute h-330 left-0 right-0 top-0 z-1 rounded-tl-3xl rounded-tr-3xl" />
+                )}
+
+                {/* logo pricing card  */}
+                <div
+                  className={clsx(
+                    "absolute left-0 right-0 z-2 flex items-center justify-center",
+                    index === 1 ? "-top-6" : "-top-6 xl:-top-11"
+                  )}
+                >
+                  <img
+                    src={plan.logo}
+                    alt={plan.title}
+                    className={clsx(
+                      "object-contain drop-shadow-2xl",
+                      index === 1 ? "size-[120px]" : "size-[88px]"
+                    )}
+                  />
+                </div>
+                {/* title pricing card  */}
+                <div
+                  className={clsx(
+                    "relative flex flex-col items-center",
+                    index === 1 ? "pt-24" : "pt-12"
+                  )}
+                >
+                  <div
+                    className={clsx(
+                      "small-2 rounded-20 relative z-2 mx-auto mb-6 border-2 px-4 py-1.5 uppercase",
+                      index === 1 ? "border-p3 text-p3 " : "border-p1 text-p1"
+                    )}
+                  >
+                    {plan.title}
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </Element>
